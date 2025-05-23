ecd6155f508bb90f0fbca856fb6556fdeb4646eb
Youngmin Jung <youngmin.jung@outlook.com>
2025-05-23 18:22:52 +0900

    Initial commit

---
+++ src/main/java/jyoungmin/finforge/FinForgeApplication.java
@@ -0,0 +1,13 @@
+package jyoungmin.finforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinForgeApplication.class, args);
+    }
+
+}
